58748c4acc37b63453826c98dace858d33ed2b08
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-01 19:57:14 -0300

    Conteúdo de operadores aritméticos

---
+++ aulas/OperadoresAritmeticos.py
@@ -0,0 +1,19 @@
+#Operadores matemáticos
+numero1 = 10
+numero2 = 10
+
+#soma
+soma = numero1+numero2
+#subtração
+subtracao= numero1-numero2
+#multiplicacao
+multiplicacao= numero1*numero2
+#divisão
+divisao=numero1/numero2
+resto=numero1//numero2
+
+print("Soma dos numeros:",soma)
+print("Subtração dos números:",subtracao)
+print("Multiplicação dos números:",multiplicacao)
+print("Divisão dos números:",divisao)
+print("Resto dos números:",resto)
