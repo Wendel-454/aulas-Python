0bc3ba1446a780cbd44146838562a3083ea6ee44
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-02 19:12:15 -0300

    Atualizando atividades

--- atividades/atividade2/Atividade2.py
+++ atividades/atividade2/Atividade2.py
@@ -3,6 +3,5 @@ nome = str(input("Digite seu nome:"))
 idade = int(input("Digite sua idade:"))
 plano = input("Tem plano?")
 resultado = plano == "sim"
-resultado= idade > 17 and idade < 65 and plano
+resultado= idade > 17 and idade < 65 and resultado == True
 print("seu nome é ",nome,",sua idade é ", idade,"você tem plano?",plano,"você foi aceito?", resultado)
-
