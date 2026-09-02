6db77d206d7e898e94677a5615be0bdcaccf336b
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-02 19:08:46 -0300

    Atualizando atividades

--- atividades/Atividade2.py
+++ atividades/Atividade2.py
@@ -1,8 +1,8 @@
 #sistema de atendimento hospitalar
-#nome = str(input("Digite seu nome:"))
-#idade = int(input("Digite sua idade:"))
+nome = str(input("Digite seu nome:"))
+idade = int(input("Digite sua idade:"))
 plano = input("Tem plano?")
 resultado = plano == "sim"
-#resultado= idade >= 18 and idade < 65 and plano == True
-#print("seu nome é ",nome,",você tem ", idade,"você tem plano?",plano,"você foi aceito?", resultado)
-print(resultado)
+resultado= idade > 17 and idade < 65 and plano
+print("seu nome é ",nome,",sua idade é ", idade,"você tem plano?",plano,"você foi aceito?", resultado)
+
