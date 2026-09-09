a3d8d0bda85585eaec68358390e935ad73dd1e2a
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-08 21:39:31 -0300

    Resolvendo questão 5

--- atividades/atividade6/questao5.py
+++ atividades/atividade6/questao5.py
@@ -3,4 +3,18 @@ Crie um programa que peça ao usuário um número inteiro para o qual ele deseja
 Utilizando uma variável de incremento (como visto no exemplo da idade), crie um laço while que vá de 1 até 10,
 exibindo o resultado da multiplicação do número escolhido pelo contador.
 Exemplo de saída esperada se o usuário digitar 5:
+*5 x 1 = 5
+*5 x 2 = 10
+*... (até 10)
+
 """
+import time
+
+numero = int(input("Digite o número do qual deseja ver a tabuada: "))
+contagem = 0
+resultado = 0
+while contagem < 10:
+    time.sleep(0.5)
+    contagem += 1
+    resultado = numero * contagem
+    print(f"{numero} x {contagem} = {resultado}")
