ba9d1d7772ca1cc4c3e37deabec5fb0d6b4b3034
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-08 21:28:06 -0300

    Corrigindo titulo da atividade 4

---
+++ atividades/atividade6/questao5.py
@@ -0,0 +1,6 @@
+"""
+Crie um programa que peça ao usuário um número inteiro para o qual ele deseja ver a tabuada.
+Utilizando uma variável de incremento (como visto no exemplo da idade), crie um laço while que vá de 1 até 10,
+exibindo o resultado da multiplicação do número escolhido pelo contador.
+Exemplo de saída esperada se o usuário digitar 5:
+"""
