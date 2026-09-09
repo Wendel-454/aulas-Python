b33a9e4633283baf99cf53b493841a7688496be9
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-08 21:47:18 -0300

    Subindo questão 6 inacabada

---
+++ atividades/atividade6/questao6.py
@@ -0,0 +1,16 @@
+"""
+Crie um programa onde o computador "pensa" em um número secreto (você pode definir um número fixo diretamente no código,
+por exemplo, numero_secreto = 14).
+O usuário deve tentar adivinhar qual é esse número.
+Usando a estrutura while, o programa deve continuar pedindo um novo palpite enquanto o usuário não acertar.
+Requisito extra: Crie uma variável para contar quantas tentativas o usuário fez.
+Quando ele finalmente acertar o número, exiba a mensagem: "Parabéns! Você acertou o número secreto em [X] tentativas!"
+(onde X é o número de vezes que ele tentou).
+
+"""
+#Resposta
+import random
+
+numero_secreto = random.randint(1, 10)
+tentativas = 6
+print
