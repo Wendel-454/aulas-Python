d32a41e460f38b6302355321f0d5f77ddad0ba00
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-11 20:43:12 -0300

    questão 1

--- atividades/atividade7/questao1.py
+++ atividades/atividade7/questao1.py
@@ -1,8 +1,9 @@
 #resposta
 add_funcionario = ""
 funcionarios = []
-while add_funcionario != "0":
+while add_funcionario != "sair":
     add_funcionario = input("Digite o nome do funcionário que deseja adicionar ou digite 'sair' para sair.\nDigite aqui:")
-    funcionarios.append(add_funcionario)
+    if add_funcionario != "sair":
+        funcionarios.append(add_funcionario)
 print(funcionarios)
 
