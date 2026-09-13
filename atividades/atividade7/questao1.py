7d408bfde3523fe7bdd337b547d41c8d33b86f9f
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-12 21:54:46 -0300

    Resolvendo questão 1

--- atividades/atividade7/questao1.py
+++ atividades/atividade7/questao1.py
@@ -3,17 +3,24 @@ import random
 
 funcionarios = []
 add_funcionario = ""
+contagem = 0
 
 while add_funcionario != "sair":
     add_funcionario = input("Digite o nome do funcionário que deseja adicionar ou digite 'sair' para sair.\nDigite aqui:")
     if add_funcionario != "sair":
         funcionarios.append(add_funcionario)
 
+print(f"Quantidade de funcionários: {len(funcionarios)}")
+
+for i in funcionarios:
+    contagem += 1
+    print(f"Funcionário {contagem}: {i}")
+
 for i in range(len(funcionarios)):
-    sorteio = random.randint(0, 1)
-    if sorteio == 0:
-        print(f"O funcionário {funcionarios[i]} será demitido")
+    faturamento = random.randint(0, 500)
+    if faturamento < 250:
+        print(f"O funcionário {funcionarios[i]} faturou este mês R${faturamento} e será demitido.")
     else:
-        print(f"O funcionário {funcionarios[i]} Receberá aumento")
+        print(f"O funcionário {funcionarios[i]} faturou este mês R${faturamento} e receberá aumento.")
 
 
