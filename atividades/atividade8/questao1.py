12403b83b0bfa983b9e091de55e0ee42ec25e4c6
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-13 18:02:09 -0300

    Resolvendo questão 1

--- atividades/atividade8/questao1.py
+++ atividades/atividade8/questao1.py
@@ -1,7 +1,15 @@
-"""
-Teste
-"""
-def função():
-    foto = int(input())
-    print("Função pra medir o tamanho da minha peça")
-    print(foto)
+
+def media():
+        nome = input("Digite seu nome: ")
+        nota1 = float(input("Digite sua nota no primeiro bimestre: "))
+        nota2 = float(input("Digite sua nota no segundo bimestre: "))
+        nota3 = float(input("Digite sua nota no terceiro bimestre: "))
+        nota4 = float(input("Digite sua nota no quarto bimestre: "))
+        calcmedia = (nota1 + nota2 + nota3 + nota4) / 4
+        if calcmedia >= 7:
+            print(f"{nome}, sua média foi de {calcmedia:.2f} e você foi aprovado.")
+        else:
+            print(f"{nome}, sua média foi de {calcmedia:.2f} e você foi reprovado.")
+
+media()
+
