3234df9446a10244e295d4e4e26b9b5cacc1dcb4
Wendel-454 <117536360+Wendel-454@users.noreply.github.com>
2026-09-12 21:41:12 -0300

    Resolvendo questão 1

--- atividades/atividade7/questao1.py
+++ atividades/atividade7/questao1.py
@@ -1,12 +1,14 @@
 #resposta
 import random
-add_funcionario = ""
+
 funcionarios = []
-sorteio = 0
+add_funcionario = ""
+
 while add_funcionario != "sair":
     add_funcionario = input("Digite o nome do funcionário que deseja adicionar ou digite 'sair' para sair.\nDigite aqui:")
     if add_funcionario != "sair":
         funcionarios.append(add_funcionario)
+
 for i in range(len(funcionarios)):
     sorteio = random.randint(0, 1)
     if sorteio == 0:
