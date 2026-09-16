9aaec18dd3648d816e22403bf2c6077b64ac956c
Wendel-454 <maikon.colab@anatel.gov.br>
2026-09-16 14:12:34 -0300

    Resolvendo questão 7

---
+++ atividades_poo/atividade1/Carro.py
@@ -0,0 +1,65 @@
+import time
+
+class Carro:
+
+    def __init__(self,categoria, modelo, cor, ano, potencia, motor):
+        self.categoria = categoria
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        self.potencia = potencia
+        self.motor = motor
+        self.ligado = False
+        self.trancado = False
+
+    def __str__(self):
+        return (f"Categoria: {self.categoria}\n"
+                f"Modelo:    {self.modelo}\n"
+                f"Cor:       {self.cor}\n"
+                f"Ano:       {self.ano}\n"
+                f"Potência:  {self.potencia}cv\n"
+                f"Motor:     {self.motor}")
+
+    def ligar(self):
+        if not self.ligado:
+            print("Girando chave de ignição...")
+            time.sleep(1)
+            print("Motor de partida acionando...")
+            time.sleep(1)
+            self.ligado = True
+            print("Motor funcionando!")
+            time.sleep(1)
+        else:
+            print("Motor já está funcionando!")
+
+    def desligar(self):
+        if not self.ligado:
+            print("Motor já está desligado!")
+        else:
+            print("Desvirando chave de ignição...")
+            time.sleep(1)
+            self.ligado = False
+            print("Motor desligado!")
+            time.sleep(1)
+
+    def trancar(self):
+        if not self.trancado:
+            print("Acionando controle de alarme...")
+            time.sleep(1)
+            print("Carro trancado!")
+        else:
+            print("Carro já trancado!")
+
+    def destrancar(self):
+        if not self.trancado:
+            print("Carro já está destrancado!")
+        else:
+            print("Acionando controle de alarme...")
+            time.sleep(1)
+            print("Carro destrancado!")
+
+chevette = Carro("Sedan","DL", "Vermelho", "1993", 81, "1.6/S (OHC)")
+gol = Carro("Hatch","GL", "Prata","1992","98","AP 1.8")
+palio = Carro("Hatch","EX", "Cinza","1998","61","Fiasa 1.0")
+opala = Carro("Grand Sedan","Diplomata SE Collectors", "Preto","1992","121", "Opala 4100")
+c10 = Carro("Caminhonete","Veraneio", "Azul","1971","151","Chevrolet 261")
